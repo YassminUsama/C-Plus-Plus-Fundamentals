4b401be5af9831cdfaf46ffac1f13a62f4b51270
YassminUsama <yassmeenusama08@gmail.com>
2026-04-21 12:17:15 +0200

    Create Circle_Class_Implementation.cpp

---
+++ Circle_Class_Implementation.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+class Circle
+{
+public:
+int radius;
+const double pi = 3.14;
+double getArea()
+{
+return pi * radius * radius;
+}
+int getDiameter()
+{
+return radius * 2;
+}
+double getCircumference()
+{
+return 2 * pi * radius;
+}
+};
+int main()
+{
+Circle c; 
+cout << "Please enter your radius: ";  
+cin >> c.radius;
+cout << "Area = " << c.getArea() << endl;
+cout << "Diameter = " << c.getDiameter() << endl;
+cout << "Circumference = " << c.getCircumference() << endl;
+return 0;
+}
